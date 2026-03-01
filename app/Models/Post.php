7a9945f9d8d344ae02b99585d13410ee94a0d695
Shivadarsh <shivadarsh777@gmail.com>
2026-03-01 19:42:30 +0530

    updated some ui

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -20,4 +20,9 @@ class Post extends Model
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -74,4 +74,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Message::class, 'receiver_id');
     }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
